bb4723ef32d2855ec2d63a50ded44fce8f771203
kirkdc <kirk_dcosta@yahoo.com>
2019-01-15 13:25:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+require('dotenv').config()
+
+const express = require ("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+//Static folders
+app.use(express.static("public"));
+app.use(express.static("frontCode"));
+
+
+
+//bodyParser middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.post("/send", function(req, res){
+    const output = `
+<p> You have a new contact request</p>
+<h3> Contact Details</h3>
+<ul> 
+    <li>Name: ${req.body.name} </li>
+    <li>email: ${req.body.email} </li>
+    <li>Subject: ${req.body.subject} </li>
+</ul>
+<h3> Message</h3>
+<p> ${req.body.message} </p>
+`;
+    
+// create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+//        host: 'smtp-mail.outlook.com',
+//        port: 587,
+        service: process.env.SERVICE,
+        secure: false, // true for 465, false for other ports
+        tls: {
+        ciphers:'SSLv3',
+        rejectUnauthorized: false //new code for outlook. remove if it doesnt work.
+    }, 
+        auth: {
+            user: process.env.USER, // generated ethereal user
+            pass: process.env.PASS // generated ethereal password
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Nodemailer Contact" <dcostacode@yahoo.com>', // sender address
+        to: 'dcostacode@gmail.com, process.env.MY_EMAIL', // list of receivers
+        subject: 'Node Contact Request', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output
+        //html: '<b>Hello world?</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info)); //DONT KNOW ABT THIS LINE OF CODE WASNT THERE IN THE EXAMPLE 
+        
+        res.render('home'); //HAVE TO CHANGE THIS I DONT HAVE A CONTACT ROUTE. should ideally go back home with flash message
+    });
+});
+
+app.listen(3000, function(){
+   console.log("SERVER IS ACTIVE!"); 
+});
---
+++ frontCode/script.js
@@ -0,0 +1,171 @@
+AOS.init(); // animate on scroll animations run
+console.log("SUP DAWG!");
+
+//========================================
+//           STICKY NAV BAR
+//======================================== 
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky", "color-change")
+  } else {
+    navbar.classList.remove("sticky", "color-change");
+  }
+};
+
+
+//========================================
+//           SMOOTH SCROLLING
+//========================================
+
+$(document).ready(function(){
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: ".navbar", offset: 60});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#navbar1 a, .scroll-link").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+});
+
+
+//========================================
+//    THIS IS MESSY NEEDS REFACTOR
+//========================================    
+
+var circle1 = new ProgressBar.Circle('.anim-html', {
+    strokeWidth: 7,  // This means 4% of the container
+    color: '#bf0000'  
+});
+
+//circle.animate(0.4);
+
+var circle2 = new ProgressBar.Circle('.anim-css', {
+    strokeWidth: 7,  // This means 4% of the container
+    color: '#45008b' 
+});
+//circle.animate(0.4);
+
+
+var circle3 = new ProgressBar.Circle('.anim-js', {
+    strokeWidth: 7,  // This means 4% of the container
+    color: 'rgb(196, 60, 0)'
+});
+//circle.animate(0.4);
+
+$(document).ready(function() {
+	var bar = $(".bar-html");
+	var pos = bar.position();	
+    
+    $(window).scroll(function() {
+		var windowpos = $(window).scrollTop();
+		if (windowpos <=980) {    //Trigger animation when user reaches 980 point while scrolling
+			circle1.animate(0), circle2.animate(0), circle3.animate(0);  //b.addClass("anim-html");
+		} else {
+			circle1.animate(0.6), circle2.animate(0.5), circle3.animate(0.3); //b.removeClass("anim-html");
+		}
+	});
+});
+
+
+//=============================================
+//  NAME AND TITLE ON MONITOR ON SCROLL EVENT
+//============================================= 
+
+function iDisappear() {
+    if ( $( window ).scrollTop() > 400 ) {
+        $("#name").addClass("disappear");
+    }
+    else {
+        $("#name").removeClass("disappear");
+    }
+}
+
+$(window).scroll(iDisappear);
+
+$(document).ready(iDisappear);
+
+//^FIX for the name appearing on page reload
+
+
+//=============================================
+//  EASTEREGG TOGGLE
+//============================================= 
+
+
+$(document).ready(function(){
+  $(".card-back").click(function(){
+    $(".para").toggleClass("woah .caption-abt");
+  });
+});
+
+//$(document).ready(function() {
+//	var n = $(".name-container");
+//	var pos = n.position();	
+//    
+//    $(window).scroll(function() {
+//		var windowpos = $(window).scrollTop();
+//		if (windowpos <=450) {    //Trigger animation when user reaches 980 point while scrolling
+//			//$(".name-container").toggleClass("disappear");//b.addClass("anim-html");
+//            $("#name").removeClass("disappear");
+//           // console.log("APPEAR")
+//		} else {
+//            $("#name").addClass("disappear");
+//			//$(".name-container").toggleClass("appear"); //b.removeClass("anim-html");
+//            // console.log("DISSAPPEAR")
+//		}
+//	});
+//});
+
+//https://www.sitepoint.com/introduction-jquery-scroll-based-animations/
+//https://stackoverflow.com/questions/13827165/element-appears-and-disappear-as-user-scroll-down-the-page
+//https://stackoverflow.com/questions/50352235/scroll-event-not-firing-on-page-reload
+
+
+
+
+
+
+//=============================================
+//  ANIM LOADING SKILL CIRCLE ON SCROLL SCRIPT
+//============================================= 
+
+
+//var $bar = $(".bar-html");
+//var $win = $(window);
+//
+//$win.on('scroll', function(){
+//   console.log($win.scrollTop());
+//});
+
+
+
+
+
